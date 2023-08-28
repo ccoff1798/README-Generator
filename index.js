26e7ecf8a03b7661f3cc8312ee79d9acefce583a
Cody <ccoff1798@gmail.com>
2023-08-28 12:37:02 -0600

    got inquirer setup started

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Enter Project Title', 'Enter Description', 'Enter Intallation Instructions','Pick License','Enter Usage Instructions', 'Enter Contributings', 'Enter Tests'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +12,32 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+//Inquierer Prompts
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: questions[0],
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: questions[1],
+      },
+      {
+        type: 'input',
+        name: 'Install',
+        message: questions[2],
+      },
+  ])
+
+  //This is residual from the code
+//   .then((data) => {
+//     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+//   });
